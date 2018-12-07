f74ba7e391a05889fd2dc4a7d7a7c35688b44856
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-12-08 02:54:20 +0500

    Project Created

---
+++ Ch19-P11-MyEBookReader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch19_P11_MyEBookReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
